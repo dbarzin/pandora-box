136d35a6ec26e9813723322603f3e1496c846448
Didier <dbarzin@gmail.com>
2022-06-28 09:53:22 +0000

    work in progress

--- tests/images.py
+++ tests/images.py
@@ -3,12 +3,12 @@
 import os
 import time
 
-os.system("convert -resize 1920x1080 -background black -gravity center -extent 1920x1080 image1.png bgra:/dev/fb0")
+os.system("convert -resize 1024x600 -background black -gravity center -extent 1024x600 image1.png bgra:/dev/fb0")
 time.sleep(1)
-os.system("convert -resize 1920x1080 -background black -gravity center -extent 1920x1080 image2.png bgra:/dev/fb0")
+os.system("convert -resize 1024x600 -background black -gravity center -extent 1024x600 image2.png bgra:/dev/fb0")
 time.sleep(1)
-os.system("convert -resize 1920x1080 -background black -gravity center -extent 1920x1080 image1.png bgra:/dev/fb0")
+os.system("convert -resize 1024x600 -background black -gravity center -extent 1024x600 image1.png bgra:/dev/fb0")
 time.sleep(1)
-os.system("convert -resize 1920x1080 -background black -gravity center -extent 1920x1080 image2.png bgra:/dev/fb0")
+os.system("convert -resize 1024x600 -background black -gravity center -extent 1024x600 image2.png bgra:/dev/fb0")
 time.sleep(1)
 
