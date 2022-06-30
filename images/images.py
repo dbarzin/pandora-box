6e974ca2dfb37d76308a7ef8a34ba86a28a7b0ab
Didier <dbarzin@gmail.com>
2022-06-30 09:34:58 +0000

    add image

--- images/images.py
+++ images/images.py
@@ -8,7 +8,7 @@ import time
 size = "1024x600"
 #size = "1920x1080"
 
-images = ["pandora-box1.png", "pandora-box2.png", "pandora-box3.png", "pandora-box4.png"]
+images = ["pandora-box1.png", "pandora-box2.png", "pandora-box3.png", "pandora-box4.png", "pandora-box5.png"]
 
 for image in images :
 	os.system("convert -resize %s -background black -gravity center -extent %s %s bgra:/dev/fb0" % (size, size, image))
