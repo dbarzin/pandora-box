248102325f4f8e6147236e69025b43180142e4c9
Didier Barzin <dbarzin@gmail.com>
2022-07-13 15:26:00 +0200

    double import

--- pandora-box.py
+++ pandora-box.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-
- # 
- # This file is part of the Pandora-box distribution (https://github.com/dbarzin/pandora-box).
- # Copyright (c) 2022 Didier Barzin.
- # 
- # This program is free software: you can redistribute it and/or modify  
- # it under the terms of the GNU General Public License as published by  
- # the Free Software Foundation, version 3.
- #
- # This program is distributed in the hope that it will be useful, but 
- # WITHOUT ANY WARRANTY; without even the implied warranty of 
- # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
- # General Public License for more details.
- #
- # You should have received a copy of the GNU General Public License 
- # along with this program. If not, see <http://www.gnu.org/licenses/>.
- #
+# 
+# This file is part of the Pandora-box distribution (https://github.com/dbarzin/pandora-box).
+# Copyright (c) 2022 Didier Barzin.
+# 
+# This program is free software: you can redistribute it and/or modify  
+# it under the terms of the GNU General Public License as published by  
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License 
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
 
 import curses
 from curses import wrapper
@@ -26,7 +25,6 @@ import pyudev
 import psutil
 import os
 import logging
-import time
 import configparser
 import shutil
 from datetime import datetime
