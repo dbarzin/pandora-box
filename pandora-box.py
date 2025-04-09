f536a5a3573b04aaf4a78145b5c995e783d3eaa7
didier <dbarzin@gmail.com>
2025-04-09 15:26:06 +0200

    fix bug

--- pandora-box.py
+++ pandora-box.py
@@ -882,7 +882,7 @@ def get_enabled_workers():
     return [file.stem for file in yml_files]
 
 def wait_for_workers():
-    target_count = get_enabled_workers() - 1
+    target_count = len(get_enabled_workers()) - 1
     while True:
         workers = self.pandora.get_enabled_workers()
         log(f"Workers ready : {len(workers)}/ {target_count}")
